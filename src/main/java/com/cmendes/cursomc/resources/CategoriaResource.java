f70266378e65a90d6c085a683e3fab8a16b7d828
Carlos Mendes <carlos.antunes.mendes@gmail.com>
2019-07-02 14:10:06 -0300

    Criando repository e service do dominio Categoria

--- src/main/java/com/cmendes/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cmendes/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,32 @@
 package com.cmendes.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cmendes.cursomc.domain.Categoria;
+import com.cmendes.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categoria")
 public class CategoriaResource {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		return "Method Catagoria";
+		Categoria obj = service.find(id);
+		
+		
+		return ResponseEntity.ok().body(obj);
 	}
 }
