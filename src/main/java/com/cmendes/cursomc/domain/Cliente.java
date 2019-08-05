541dc37367a546b771c74a5f43505360b0732cf0
Carlos Mendes <carlos.antunes.mendes@gmail.com>
2019-08-05 12:05:28 -0300

    Craido os domains Pedido, Pagamento, PagamentoComBoleto e PagamentoComCartao

--- src/main/java/com/cmendes/cursomc/domain/Cliente.java
+++ src/main/java/com/cmendes/cursomc/domain/Cliente.java
@@ -38,6 +38,7 @@ public class Cliente implements Serializable{
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente() {
 	}
@@ -97,6 +98,14 @@ public class Cliente implements Serializable{
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -122,6 +131,5 @@ public class Cliente implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
+
 }
---
+++ src/main/java/com/cmendes/cursomc/domain/PagamentoComCartao.java
@@ -0,0 +1,27 @@
+package com.cmendes.cursomc.domain;
+
+import com.cmendes.cursomc.domain.enums.EstadoPagamento;
+
+public class PagamentoComCartao extends Pagamento{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer numeroDeParcelas;
+	
+	public PagamentoComCartao() {
+	}
+	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
+		super(id, estado, pedido);
+		this.setNumeroDeParcelas(numeroDeParcelas);
+	}
+	
+	public Integer getNumeroDeParcelas() {
+		return numeroDeParcelas;
+	}
+	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+	
+	
+	
+}
